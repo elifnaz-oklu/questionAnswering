699e908d6afd7358e730a9c4ea0bcef429152f00
elifnaz.oklu <elifnaz232@gmail.com>
2019-03-04 02:26:37 +0300

    initial commit

---
+++ src/com/company/Game.java
@@ -0,0 +1,114 @@
+package com.company;
+
+
+import java.util.Collections;
+import java.util.List;
+
+public class Game {
+
+    private int year;
+    private String name;
+    private double score;
+    private String type;
+    private int playerNumber;
+    private String company;
+    private int price;
+    private List<String> shops;
+
+
+    public Game(String name,int year,double score,String type,int playerNumber,String company,int price){
+
+        this.company=company;
+        this.name=name;
+        this.playerNumber=playerNumber;
+        this.price=price;
+        this.score=score;
+        this.type=type;
+        this.year=year;
+        this.shops = Collections.emptyList();
+
+    }
+
+    public Game(String name,int year,double score,String type,int playerNumber,String company,int price,List shops){
+
+        this.company=company;
+        this.name=name;
+        this.playerNumber=playerNumber;
+        this.price=price;
+        this.score=score;
+        this.type=type;
+        this.year=year;
+        this.shops=shops;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Oyunun adı "+ getName() + ",çıkış tarihi " + getYear() + ", oyunun türü "+  ", oyuncu sayısı " + getPlayerNumber() + ",puanı "+ getScore()+
+                ", oyunu çıkaran şirket " + getCompany() + ",oyunun fiyatı "+ getPrice();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public List<String> getShops() {
+        return shops;
+    }
+
+    public void setShops(List<String> shops) {
+        this.shops = shops;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,125 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean c = true;
+        List<Game> games = new ArrayList<Game>();
+        Game g1 = new Game("game1",2015,8.1,"adventure",1,"company1",100);
+        Game g2 = new Game("game2",2010,7.1,"strategy",2,"company1",50,Arrays.asList("shop1", "shop2"));
+        Game g3 = new Game("game3",2020,8.9,"sci-fi",1,"company2",30);
+        Game g4 = new Game("game4",2008,6.2,"adventure",4,"company1",200,Arrays.asList("shop1"));
+        Game g5 = new Game("game5",2013,4.2,"action",3,"company4",20,Arrays.asList("shop3"));
+
+        games.add(g1);
+        games.add(g2);
+        games.add(g3);
+        games.add(g4);
+        games.add(g5);
+
+        do {
+            System.out.print("\nLütfen sorunuzu giriniz: ");
+            Scanner reader = new Scanner(System.in);
+            String s = reader.nextLine();
+            s = s.toLowerCase();
+
+
+            if (s.contentEquals("tarihe göre oyun göster")) {
+                System.out.print("Lütfen tarih aralığı giriniz: ");
+                int t1 = reader.nextInt();
+                int t2 = reader.nextInt();
+                for (Game g : games) {
+                    if (t1 <= g.getYear() & g.getYear() <= t2) {
+                        System.out.println(g.getName());
+                    }
+                }
+
+            }
+
+            if (s.contentEquals("puana göre oyun göster")) {
+                System.out.print("Oyun kaç puan üstü olsun : ");
+                double t1 = reader.nextDouble();
+                for (Game g : games) {
+                    if (t1 <= g.getScore()) {
+                        System.out.println(g.getName());
+                    }
+                }
+
+            }
+
+            if (s.contentEquals("oyuncu sayısına göre oyun göster")) {
+                System.out.print("Oyun kaç kişi ve üstü olsun : ");
+                double t1 = reader.nextInt();
+                for (Game g : games) {
+                    if (t1 <= g.getPlayerNumber()) {
+                        System.out.println(g.getName());
+                    }
+                }
+
+            }
+
+            if (s.contentEquals("fiyata göre oyun göster")) {
+                System.out.print("Lütfen fiyat aralığı giriniz: ");
+                int t1 = reader.nextInt();
+                int t2 = reader.nextInt();
+                for (Game g : games) {
+                    if (t1 <= g.getPrice() & g.getPrice() <= t2) {
+                        System.out.println(g.getName() + " oyununun fiyatı " + g.getPrice());
+                    }
+                }
+
+            }
+
+
+            if (s.contentEquals("oyunun satıldığı yerleri göster")) {
+                System.out.print("Oyun adı giriniz: ");
+                String s1 = reader.nextLine().toLowerCase();
+                for (Game g : games) {
+                    if (s1.contentEquals(g.getName()) & !g.getShops().isEmpty()) {
+
+                        for(String shop : g.getShops())
+                        System.out.println(shop);
+                    }
+                    else if (s1.contentEquals(g.getName()) & g.getShops().isEmpty()){
+                        System.out.println("Oyun  satışta değildir");
+                    }
+                }
+
+            }
+
+
+            for (Game g : games) {
+
+                if (s.contains(g.getName()) & (s.contains("tarih") | s.contains("yıl"))) {
+                    System.out.println(g.getName() + " adlı oyunun çıkış tarihi " + g.getYear());
+                } else if (s.contains(g.getName())) {
+                    System.out.println("Oyunun bilgileri: \n" + g.toString());
+                }
+
+                if (s.contains(g.getType())) {
+                    System.out.println(g.getName());
+
+                }
+
+                if (s.contains(g.getCompany())) {
+                    System.out.println(g.getName());
+
+                }
+
+            }
+
+            System.out.print("Başka soru sormak istiyor musunuz: ");
+            c = reader.nextBoolean();
+
+        }while(c);
+
+
+
+        }
+    }
