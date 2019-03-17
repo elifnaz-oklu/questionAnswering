035eb1a4b23c48d7923b772f236418c8c21692e0
elifnaz.oklu <elifnaz232@gmail.com>
2019-03-17 20:43:11 +0300

    made changes

--- src/com/company/Question.java
+++ src/com/company/Question.java
@@ -30,8 +30,8 @@ public class Question {
 
             String pattern1 = "(.*)" + i.toLowerCase() + "( )(.*)(tarih|yıl|zaman)(.*)";
             String pattern2 = "(.*)" + i.toLowerCase() + "( )(.*)(bilgi|nedir|nasıl)(.*)";
-            String pattern3 = "(.*)" + games.get(i).getType().toLowerCase() + "(.*)";
-            String pattern4 = "(.*)" + games.get(i).getCompany().toLowerCase() + "(.*)";
+            String pattern3 = "(.*)" + games.get(i).getType().toLowerCase() + "(.*)(tür|tip|çeşit)(.*)";
+            String pattern4 = "(.*)" + games.get(i).getCompany().toLowerCase() + "(.*)(şirket|firma)(.*)";
             String pattern9 = "(.*)" + i.toLowerCase() + "( )(.*)(nerede|nereden|hangi)(.*)(satılıyor|satın|satış)(.*)";
 
             Pattern r1 = Pattern.compile(pattern1);
